9f4eee91be8ca80c898f66b92a2358faee8124df
Paul E <peng7419@gmail.com>
2018-04-15 20:49:28 +0930

    styling Notes

---
+++ app/components/Notes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Note from './Note';
+import Editable from './Editable';
+
+export default ({
+	notes,
+	onDelete = () => { },
+	onNoteClick = () => { },
+	onEdit = () => {}
+}) => (
+	<ul className="notes">
+		{notes.map(({id, editing, task}) =>
+			<li key={id}>
+					<Note
+						className="note"	
+						onClick={onNoteClick.bind(null, id)}>
+						<Editable
+							editing={editing}	
+							value={task}
+							onEdit={onEdit.bind(null, id)}
+						/>	
+						<button
+							className="delete"	
+							onClick={onDelete.bind(null, id)}>x</button>
+			</Note>		
+			</li>
+		)}
+	</ul>
+);
